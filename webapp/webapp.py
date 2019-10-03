18ce3f90a3c1e1302208aed3d70ac0c63e4349eb
bearsun <sunliwei.china@gmail.com>
2019-10-03 19:48:39 -0400

    updated webapp

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -7,7 +7,8 @@ tfidf_nb_app = load('tfidf_nb_app.joblib')
 
 @app.route('/') #we are now using these methods to get user input
 def home_page():
-	return render_template('index.html')  # render a template
+	return render_template('index.html', team1 = 'NBA',
+      team2 = 'NBA', team3 = 'NBA')  # render a template
 
 @app.route('/output', methods=['GET', 'POST'])
 def home_post():
@@ -15,13 +16,20 @@ def home_post():
   text = request.args.get('user_input')
   # Case if empty
   if text =='':
-    return render_template("index.html", team = '')
+    return render_template("index.html", team1 = 'NBA',
+      team2 = 'NBA', team3 = 'NBA')
   else:
     ptext = tfidf.transform([text])
-    pred_team = tfidf_nb_app.predict(ptext)[0]
-    return render_template("index.html", team = pred_team)
+    probs = tfidf_nb_app.predict_proba(ptext)
+    ktop3 = probs[0].argsort()[-3:][::-1]
+    teams = tfidf_nb_app.classes_[ktop3]
+    prob3 = list(probs[0][ktop3]*100)
+    return render_template("index.html", team1 = teams[0],
+      team2 = teams[1], team3 = teams[2],
+      prob1 = round(prob3[0], 2), prob2 = round(prob3[1], 2),
+      prob3 = round(prob3[2], 2))
 
 
 # start the server with the 'run()' method
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
